cd875e1a19093da641f6cc5ce222f4075e52dd34
Agha Naveed <naveedabs31@gmail.com>
2024-12-27 13:04:15 +0500

    making navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import LogoComponent from '../extraComponents/Logo'
 import { Link } from 'react-router'
+import { IoSearch } from "react-icons/io5";
 
 export default function navbar() {
   return (
-    <div>
+    <div className='flex'>
       <LogoComponent />
 
       <nav>
-        <ul>
+        <ul className='flex'>
           <li><Link to={"/"}>Home</Link></li>
           <li><Link to={"/a"}>Skills</Link></li>
           <li><Link to={"/b"}>Projects</Link></li>
         </ul>
       </nav>
+
+      <div className="search-bar flex">
+        <input type="text" className='h-[56px] bg-light-gray' />
+        <IoSearch />
+      </div>
     </div>
   )
 }
