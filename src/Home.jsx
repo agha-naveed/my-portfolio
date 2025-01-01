ba0882946e50b3a68aeaf590869e1d2e476bb0e1
Agha Naveed <naveedabs31@gmail.com>
2025-01-01 23:08:53 +0500

    .

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Skills from './components/Skills'
+import Projects from './components/Projects'
 
 export default function Home() {
     return (
         <div className='w-full h-full'>
             <Header />
             <Skills />
+            <Projects />
 
         </div>
     )
