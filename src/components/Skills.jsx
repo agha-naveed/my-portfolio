3c945c7cbbc8cd491dac4ca4ada46c7d1bbd7b47
agha-naveed <naveedabs31@gmail.com>
2025-01-08 12:01:12 +0500

    .

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -68,8 +68,8 @@ export default function Skills() {
       <div className="container mx-auto justify-items-center">
 
         <section className='w-[80%] py-20'>
-          <h1 className='text-white font-poppins-semibold text-[38px] py-1'>About</h1>
-          <div className='flex gap-4'>
+          <h1 className='text-white font-poppins-semibold text-[38px] py-1' data-aos="fade-up" data-aos-duration="700" >About</h1>
+          <div className='flex gap-4' data-aos="fade-up" data-aos-duration="600">
             <span className='md:block hidden h-[3px] w-[120px] bg-yellow-text relative top-[10px]'></span>
             <p className='text-white text-justify text-xl'>
               Syed Naveed Abbas is a MERN stack developer from Skardu, Pakistan, <span className='text-yellow-text'> with expertise in MongoDB, Express.js, React, and Node.js</span>. Passionate about technology, he is committed to continuous learning and staying up-to-date with new advancements. Syed's focus on creating dynamic, efficient web applications drives his growth in the ever-evolving field of <span className='text-yellow-text'> software development.</span>
