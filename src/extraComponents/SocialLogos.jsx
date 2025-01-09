42d4203a6bd6da05bfffe6cd12f684293dc5a07c
agha-naveed <naveedabs31@gmail.com>
2025-01-09 14:26:42 +0500

    /

--- src/extraComponents/SocialLogos.jsx
+++ src/extraComponents/SocialLogos.jsx
@@ -8,11 +8,16 @@ export default function SocialLogos() {
     <div className='justify-items-center content-end grid gap-5 ml-10'>
         <div className='w-[3px] h-24 bg-yellow-text'></div>
         <div className='text-white text-[27px] grid gap-5'>
-            <FaFacebookF className='transition-all cursor-pointer hover:text-[#316FF6]' />
-            <FaLinkedinIn className='transition-all cursor-pointer hover:text-[#0077B5]' />
+          <div className='transition-all cursor-pointer w-10 h-10 rounded-full p-2 hover:bg-[#316FF6]'>
+            <FaFacebookF className='hover:text-white' />
+          </div>
+          <div className='transition-all cursor-pointer hover:bg-[#0077B5] w-10 h-10 rounded-full flex justify-center items-center'>
+            <FaLinkedinIn className='text-white' />
+          </div>
+
             <PiXLogoBold className='transition-all cursor-pointer hover:text-black' />
-            <FaInstagram className='"' />
-            <FiGithub className='' />
+            <FaInstagram className='transition-all cursor-pointer' />
+            <FiGithub className='transition-all cursor-pointer' />
         </div>
     </div>
   )
