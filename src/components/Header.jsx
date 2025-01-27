53d352b456bb70633a40b04da0d60f694bc3c35c
agha-naveed <naveedabs31@gmail.com>
2025-01-27 22:43:29 +0500

    contct me btn animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
 
         <header className='lg:w-[95%] md:p-0 p-10 w-full h-full justify-center lg:flex grid relative z-10'>
           
-          <div data-aos="fade-up-right" data-aos-duration="600" className='content-center place-self-center'>
+          <div data-aos="fade-up-right" data-aos-duration="600" className='grid content-center place-self-center'>
             <span className='flex place-self-center'>
               <h4 className='text-white font-poppins-light md:text-2xl text-xl'>I'M</h4>
               <h1 className='text-yellow-text lg:text-[60px] md:text-[40px] text-[34px] font-poppins-medium lg:w-[90%] w-full'>Agha Naveed</h1>
@@ -26,7 +26,7 @@ export default function Header() {
               <h3 className='hover-text text-white font-poppins-regular md:text-[32px] text-[25px] lg:text-left text-center mb-8'>Web Developer / Programmer</h3>
             </span>
             
-            <Link className='border-[5px] border-yellow-text transition-all font-poppins-regular text-yellow-text md:text-[22px] text-[22px] rounded-lg px-6 py-3 mt-5 justify-self-center lg:inline grid'>Contact Me</Link>
+            <Link className='contact-btn w-fit place-self-start relative overflow-hidden hover:text-[#272829] z-30 border-[5px] border-yellow-text font-poppins-regular text-yellow-text md:text-[22px] text-[22px] rounded-lg px-6 py-3 mt-5 grid'>Contact Me</Link>
             
           </div>
           
