faf6bc722078eb4f3a43f089957d79da72a45e0d
agha-naveed <naveedabs31@gmail.com>
2025-01-27 12:05:31 +0500

    .

--- src/extraComponents/Cursor.jsx
+++ src/extraComponents/Cursor.jsx
@@ -1,32 +1,31 @@
 import React, { useEffect, useState } from 'react'
 
 export default function Cursor() {
-    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
-    
-    useEffect(() => {
-        const updateCursorPosition = (e) => {
-            setCursorPosition({ x: e.clientX, y: e.clientY });
-          };
-      
-          window.addEventListener('mousemove', updateCursorPosition);
-      
-          return () => {
-            window.removeEventListener('mousemove', updateCursorPosition);
-          };
-    }, [])
-  return (
-    <div className='pointer-events-none'>
-        <div style={{
-            left: `${cursorPosition.x}px`,
-            top: `${cursorPosition.y}px`,
-        }} className='fixed top-0 left-0 z-[4000] w-6 h-6 rounded-full border border-white'></div>
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [cursorStyle, setCursorStyle] = useState({ x: 0, y: 0 });
 
-            <div style={{
-                left: `${cursorPosition.x}px`,
-                top: `${cursorPosition.y}px`,
-                transform: "translate(50%, 50%)"
-            }} className='fixed top-0 left-0 z-[4000] w-[9px] h-[9px] bg-white rounded-full'></div>
+  useEffect(() => {
+    const updateCursorPosition = (e) => {
+      setCursorPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener('mousemove', updateCursorPosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateCursorPosition);
+    };
+  }, []);
+
+  return (
+    <div className="pointer-events-none">
+      <div
+        style={{
+          left: `${cursorPosition.x}px`,
+          top: `${cursorPosition.y}px`,
+          transform: "translate(80%, 80%)",
+        }}
+        className="fixed top-0 left-0 z-[4000] w-3 h-3 bg-white rounded-full mix-blend-difference"
+      ></div>
     </div>
-    
-  )
+  );
 }
