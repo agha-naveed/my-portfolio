c7f81d7004d90a30fe22c63e1e35eb94808893b7
agha-naveed <naveedabs31@gmail.com>
2025-01-28 17:26:09 +0500

    added project

--- src/extraComponents/OtherProject.jsx
+++ src/extraComponents/OtherProject.jsx
@@ -57,7 +57,7 @@ export default function OtherProject() {
             <ul className='flex flex-col gap-4'>
                 <li><h3 className='text-[24px] text-yellow-text font-poppins-semibold relative left-[-18px]'>Random</h3></li>
                 <li className='list-[circle] text-white font-poppins-regular'>
-                    <Link to="https://agha-naveed.github.io/weather-app/" className='z-[200] group relative hover:text-yellow-text hover-text hover:px-[10px] transition-all' target='_blank' onMouseMove={handleMouseMove}>
+                    <Link to="https://agha-naveed.github.io/old-portfolio/" className='z-[200] group relative hover:text-yellow-text hover-text hover:px-[10px] transition-all' target='_blank' onMouseMove={handleMouseMove}>
                         Old Portfolio
                         <div className='absolute top-6 hover:hidden left-10 w-[200px] z-[200]'>
                             <img src={oldPortfolio} className='w-full hidden group-hover:block' alt="Weather App Image" style={{
