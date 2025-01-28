58712239c1b0c70c64cae0d2b55730f4037855ed
agha-naveed <naveedabs31@gmail.com>
2025-01-28 10:09:59 +0500

    cursor show with the user device

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
@@ -6,10 +6,29 @@ import ProjectGallery from './components/ProjectGallery';
 import Cursor from './extraComponents/Cursor';
 
 export default function Home() {
+    const [deviceType, setDeviceType] = useState('');
+        
+    useEffect(() => {
+        const userAgent = navigator.userAgent;
+        console.log(userAgent)
+        if (/mobile/i.test(userAgent)) {
+            setDeviceType('mobile');
+        }
+        else if (/tablet/i.test(userAgent)) {
+            setDeviceType('tablet');
+        }
+        else if (/linux/i.test(userAgent) || /windows/i.test(userAgent) || /macintosh/i.test(userAgent)) {
+            setDeviceType('desktop');
+        } else {
+            setDeviceType('Unknown');
+        }
+    }, [])
 
     return (
         <div className='w-full md:h-full'>
-            <Cursor />
+            <div className={`${deviceType == "desktop" ? "block" : "hidden"}`}>
+                <Cursor />
+            </div>
             <Header />
             <Skills />
             <Projects />
