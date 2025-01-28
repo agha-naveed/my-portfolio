2c640d960e40ee18c54d9883e86b1cad954c93b6
agha-naveed <naveedabs31@gmail.com>
2025-01-28 14:15:06 +0500

    .

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Cursor from './extraComponents/Cursor';
+import Footer from './components/Footer';
+
 
 export default function Home() {
     const [deviceType, setDeviceType] = useState('');
@@ -31,6 +33,7 @@ export default function Home() {
             <Header />
             <Skills />
             <Projects />
+            <Footer />
         </div>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className='relative'>
+            <div className='w-full h-[100px] bg-light-gray'>
+                <div className='flex items-center gap-14 relative -top-5'>
+                    <div className='w-full h-1 bg-yellow-text'></div>
+                    <div className='text-4xl text-white font-poppins-semibold'>Contact</div>
+                    <div className='w-full h-1 bg-yellow-text'></div>
+                </div>
+                <div className='container mx-auto'>
+
+                </div>
+            </div>
+        </footer>
+    )
+}
