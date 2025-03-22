fd84e65c53817319ec3e7b6a560c363fb0ccf584
agha-naveed <naveedabs31@gmail.com>
2025-03-22 16:21:11 +0500

    .

--- src/extraComponents/OtherProject.jsx
+++ src/extraComponents/OtherProject.jsx
@@ -6,6 +6,7 @@ import passStrength from '../assets/img/project-hover-img/pass_strength.webp'
 import oldPortfolio from '../assets/img/project-hover-img/old_portfolio.webp'
 
 export default function OtherProject() {
+    const [hover, setHover] = useState(false)
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
     const handleMouseMove = (e) => {
@@ -23,8 +24,8 @@ export default function OtherProject() {
                 <li className='list-[circle] text-white font-poppins-regular relative hover:z-[200]'>
                     <Link to="https://agha-naveed.github.io/weather-app/" className='z-[10] group relative hover:text-yellow-text hover-text hover:px-[10px] transition-all' target='_blank' onMouseMove={handleMouseMove}>
                         Weather App
-                        <div className='absolute top-6 hover:hidden left-10 w-[200px] z-[200]'>
-                            <img src={weatherApp} className='w-full hidden group-hover:block' alt="Weather App Image" style={{
+                        <div className={`absolute top-6 hover:hidden left-10 w-0 transition-all h-[112.5px] group-hover:w-[200px] z-[200]`}>
+                            <img src={weatherApp} className='w-full h-full object-cover' alt="Weather App Image" style={{
                                 transform: `translate(${(mousePosition.x / 20) * 5}%, ${(mousePosition.y / 20) * 5}%)`,
                                 }} />
                         </div>
@@ -34,8 +35,8 @@ export default function OtherProject() {
                 <li className='list-[circle] text-white font-poppins-regular relative hover:z-[200]'>
                     <Link to="https://agha-naveed.github.io/twitter/" className='z-[10] group relative hover:text-yellow-text hover-text hover:px-[10px] transition-all' target='_blank' onMouseMove={handleMouseMove}>
                         Twitter Clone
-                        <div className='absolute top-6 hover:hidden left-10 w-[200px] z-[200]'>
-                            <img src={twitterClone} className='w-full hidden group-hover:block' alt="Twitter Clone" style={{
+                        <div className='absolute top-6 hover:hidden left-10 w-0 transition-all h-[112.5px] group-hover:w-[200px] z-[200]'>
+                            <img src={twitterClone} className='w-full h-full object-cover' alt="Twitter Clone" style={{
                                 transform: `translate(${(mousePosition.x / 20) * 5}%, ${(mousePosition.y / 20) * 5}%)`,
                                 }} />
                         </div>
@@ -45,8 +46,8 @@ export default function OtherProject() {
                 <li className='list-[circle] text-white font-poppins-regular relative hover:z-[200]'>
                     <Link to="https://agha-naveed.github.io/password-strength-checker/" className='z-[10] group relative hover:text-yellow-text hover-text hover:px-[10px] transition-all' target='_blank' onMouseMove={handleMouseMove}>
                         Password Strength Checker
-                        <div className='absolute top-6 hover:hidden left-10 w-[200px] z-[200]'>
-                            <img src={passStrength} className='w-full hidden group-hover:block' alt="Password Strength Checker" style={{
+                        <div className='absolute top-6 hover:hidden left-10 w-0 transition-all h-[112.5px] group-hover:w-[200px] z-[200]'>
+                            <img src={passStrength} className='w-full h-full object-cover' alt="Password Strength Checker" style={{
                                 transform: `translate(${(mousePosition.x / 20) * 5}%, ${(mousePosition.y / 20) * 5}%)`,
                                 }} />
                         </div>
@@ -59,8 +60,8 @@ export default function OtherProject() {
                 <li className='list-[circle] text-white font-poppins-regular relative hover:z-[200]'>
                     <Link to="https://agha-naveed.github.io/old-portfolio/" className='z-[10] group relative hover:text-yellow-text hover-text hover:px-[10px] transition-all' target='_blank' onMouseMove={handleMouseMove}>
                         Old Portfolio
-                        <div className='absolute top-6 hover:hidden left-10 w-[200px] z-[200]'>
-                            <img src={oldPortfolio} className='w-full hidden group-hover:block' alt="Agha Naveed Old Portfolio" style={{
+                        <div className='absolute top-6 hover:hidden left-10 w-0 transition-all h-[112.5px] group-hover:w-[200px] z-[200]'>
+                            <img src={oldPortfolio} className='w-full h-full object-cover' alt="Agha Naveed Old Portfolio" style={{
                                 transform: `translate(${(mousePosition.x / 20) * 5}%, ${(mousePosition.y / 20) * 5}%)`,
                                 }} />
                         </div>
