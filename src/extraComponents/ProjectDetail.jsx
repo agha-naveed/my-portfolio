03835a63d531e202289731488166ab8e3c0a9c76
agha-naveed <naveedabs31@gmail.com>
2025-03-22 15:17:55 +0500

    .

--- src/extraComponents/ProjectDetail.jsx
+++ src/extraComponents/ProjectDetail.jsx
@@ -86,8 +86,8 @@ export default function ProjectDetail() {
   return (
     <>
       {
-        // project == "/assets/sevenup-owal1RN2.webp" ?
-        project == "/src/assets/img/projects/sevenup.webp" ?
+        project == "/assets/sevenup-owal1RN2.webp" ?
+        // project == "/src/assets/img/projects/sevenup.webp" ?
         <div className='w-full min-h-full bg-black overflow-x-hidden flex relative z-[4000]'>
           <div ref={ref} className={`min-h-screen bg-yellow-text c-trans`}></div>
           <div className='p-7 flex md:flex-row flex-col-reverse gap-10 w-full overflow-x-hidden'>
@@ -141,8 +141,8 @@ export default function ProjectDetail() {
           </div>
         </div>
         :
-        // project == "/assets/store-JUvzdpkM.webp" ?
-        project == "/src/assets/img/projects/store.webp" ?
+        project == "/assets/store-JUvzdpkM.webp" ?
+        // project == "/src/assets/img/projects/store.webp" ?
         <div className='w-screen min-h-full bg-black overflow-x-hidden flex relative z-[4000]'>
           <div ref={ref} className={`h-screen bg-yellow-text c-trans`}></div>
           <div className='p-7 flex md:flex-row flex-col-reverse gap-10 w-full overflow-y-scroll overflow-x-hidden'>
@@ -205,8 +205,8 @@ export default function ProjectDetail() {
           </div>
         </div>
         :
-        // project == "/assets/music_player-C9DeQ6Rj.webp" ?
-        project == "/src/assets/img/projects/music_player.webp" ?
+        project == "/assets/music_player-C9DeQ6Rj.webp" ?
+        // project == "/src/assets/img/projects/music_player.webp" ?
         <div className='w-full min-h-full bg-black overflow-x-hidden flex relative z-[4000]'>
           <div ref={ref} className={`min-h-screen bg-yellow-text c-trans`}></div>
           <div className='p-7 flex md:flex-row flex-col-reverse gap-10 w-full overflow-x-hidden'>
@@ -260,7 +260,8 @@ export default function ProjectDetail() {
           </div>
         </div>
         :
-        project == "/src/assets/img/projects/pharmacy.webp?t=1742547051237" ?
+        // project == "/src/assets/img/projects/pharmacy.webp?t=1742547051237" ?
+        project == "/assets/pharmacy-B4qFNTXm.webp" ?
         <div className='w-full min-h-full bg-black overflow-x-hidden flex relative z-[4000]'>
           <div ref={ref} className={`min-h-screen bg-yellow-text c-trans`}></div>
           <div className='p-7 flex md:flex-row flex-col-reverse gap-10 w-full overflow-x-hidden'>
